8a11be238ebf05e3225eaf90ba8b5416d47a9a97
Matt Baier <mbaier06@gmail.com>
2021-02-08 17:44:17 -0600

    solving lab

--- lab/week5.js
+++ lab/week5.js
@@ -40,7 +40,7 @@ window.addEventListener('DOMContentLoaded', function() {
     
     let response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=4498add9fff045f6bb873226212701&q=${location}&days=3`)
     let json = await response.json()
-    console.log(json)
+    // console.log(json)
     
     let forecast = json.forecast.forecastday
     for (i = 0; i < forecast.length; i++) {
@@ -48,7 +48,149 @@ window.addEventListener('DOMContentLoaded', function() {
       console.log(dailyForecast);
       document.querySelector('.forecast').insertAdjacentHTML('beforeend', forecastHTML(dailyForecast))
     }
-
   })
+  let losAngeles = document.querySelector('#los-angeles-forecast')
+  losAngeles.addEventListener('click', async function(event){
+    event.preventDefault()
+    
+    let location = 'Los Angeles' 
+    document.querySelector('.forecast-header').innerHTML = `${location} forecast`
+    
+    let response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=4498add9fff045f6bb873226212701&q=${location}&days=3`)
+    let json = await response.json()
+    // console.log(json)
+    
+    let forecast = json.forecast.forecastday
+    for (i = 0; i < forecast.length; i++) {
+      let dailyForecast = forecast[i]
+      console.log(dailyForecast);
+      document.querySelector('.forecast').insertAdjacentHTML('beforeend', forecastHTML(dailyForecast))
+    }
+  })
+  let paris = document.querySelector('#paris-forecast')
+  paris.addEventListener('click', async function(event){
+    event.preventDefault()
+    
+    let location = 'Paris' 
+    document.querySelector('.forecast-header').innerHTML = `${location} forecast`
+    
+    let response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=4498add9fff045f6bb873226212701&q=${location}&days=3`)
+    let json = await response.json()
+    // console.log(json)
+    
+    let forecast = json.forecast.forecastday
+    for (i = 0; i < forecast.length; i++) {
+      let dailyForecast = forecast[i]
+      console.log(dailyForecast);
+      document.querySelector('.forecast').insertAdjacentHTML('beforeend', forecastHTML(dailyForecast))
+    }
+  })
+  let moscow = document.querySelector('#moscow-forecast')
+  moscow.addEventListener('click', async function(event){
+    event.preventDefault()
+    
+    let location = 'Moscow' 
+    document.querySelector('.forecast-header').innerHTML = `${location} forecast`
+    
+    let response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=4498add9fff045f6bb873226212701&q=${location}&days=3`)
+    let json = await response.json()
+    // console.log(json)
+    
+    let forecast = json.forecast.forecastday
+    for (i = 0; i < forecast.length; i++) {
+      let dailyForecast = forecast[i]
+      console.log(dailyForecast);
+      document.querySelector('.forecast').insertAdjacentHTML('beforeend', forecastHTML(dailyForecast))
+    }
+  })
+  let NYC = document.querySelector('#new-york-city-forecast')
+  NYC.addEventListener('click', async function(event){
+    event.preventDefault()
+    
+    let location = 'New York City' 
+    document.querySelector('.forecast-header').innerHTML = `${location} forecast`
+    
+    let response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=4498add9fff045f6bb873226212701&q=${location}&days=3`)
+    let json = await response.json()
+    // console.log(json)
+    
+    let forecast = json.forecast.forecastday
+    for (i = 0; i < forecast.length; i++) {
+      let dailyForecast = forecast[i]
+      console.log(dailyForecast);
+      document.querySelector('.forecast').insertAdjacentHTML('beforeend', forecastHTML(dailyForecast))
+    }
+  })
+  let rome = document.querySelector('#rome-forecast')
+  rome.addEventListener('click', async function(event){
+    event.preventDefault()
+    
+    let location = 'Rome' 
+    document.querySelector('.forecast-header').innerHTML = `${location} forecast`
+    
+    let response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=4498add9fff045f6bb873226212701&q=${location}&days=3`)
+    let json = await response.json()
+    // console.log(json)
+    
+    let forecast = json.forecast.forecastday
+    for (i = 0; i < forecast.length; i++) {
+      let dailyForecast = forecast[i]
+      console.log(dailyForecast);
+      document.querySelector('.forecast').insertAdjacentHTML('beforeend', forecastHTML(dailyForecast))
+    }
+  })
+  let tokyo = document.querySelector('#tokyo-forecast')
+  tokyo.addEventListener('click', async function(event){
+    event.preventDefault()
+    
+    let location = 'Tokyo' 
+    document.querySelector('.forecast-header').innerHTML = `${location} forecast`
+    
+    let response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=4498add9fff045f6bb873226212701&q=${location}&days=3`)
+    let json = await response.json()
+    // console.log(json)
+    
+    let forecast = json.forecast.forecastday
+    for (i = 0; i < forecast.length; i++) {
+      let dailyForecast = forecast[i]
+      console.log(dailyForecast);
+      document.querySelector('.forecast').insertAdjacentHTML('beforeend', forecastHTML(dailyForecast))
+    }
+  })
+  let singapore = document.querySelector('#singapore-forecast')
+  singapore.addEventListener('click', async function(event){
+    event.preventDefault()
+    
+    let location = 'Singapore' 
+    document.querySelector('.forecast-header').innerHTML = `${location} forecast`
+    
+    let response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=4498add9fff045f6bb873226212701&q=${location}&days=3`)
+    let json = await response.json()
+    // console.log(json)
+    
+    let forecast = json.forecast.forecastday
+    for (i = 0; i < forecast.length; i++) {
+      let dailyForecast = forecast[i]
+      console.log(dailyForecast);
+      document.querySelector('.forecast').insertAdjacentHTML('beforeend', forecastHTML(dailyForecast))
+    }
+  })
+  // 6. add a submit event listener to the form
+  //    a. get the user-entered location from the input
+  //    b. find the forecast header (use the selector .forecast-header) and modify its innerHTML to `${location} Forecast`
+  //    c. fetch the api response from https://api.weatherapi.com/v1/forecast.json?key=YOUR_KEY&q=LOCATION&days=3
+  //    d. extract the json using the .json() function
+  //    e. find the array of daily forecast data, loop through it and, insert HTML with each day's forecast using the forecastHTML() function
+  let nameInput = document.querySelector('#location')
+  document.querySelector('.forecast-header').innerHTML = `${nameInput} forecast`
+  let response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=4498add9fff045f6bb873226212701&q=${nameInput}&days=3`)
+    let json = await response.json()
 
+    let forecast = json.forecast.forecastday
+    for (i = 0; i < forecast.length; i++) {
+      let dailyForecast = forecast[i]
+      console.log(dailyForecast);
+      document.querySelector('.forecast').insertAdjacentHTML('beforeend', forecastHTML(dailyForecast))
+    }
+  })
 })
